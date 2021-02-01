9f12d764eb4f4a878eb3df0dcbf6cd4ba54009d1
Kotaro666-dev <kotaro0726@gmail.com>
2021-02-02 07:47:49 +0900

    Update: Remove header

--- chapter01/test.cpp
+++ chapter01/test.cpp
@@ -1,21 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/02 07:22:48 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/02/02 07:39:00 by kkamashi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <bits/stdc++.h>
 #define PI 3.1415926535897
 using namespace std;
 typedef long long ll;
 const int INF = 1000000000;
-const ll LINF = 1000000000000000000;//1e18
+const ll LINF = 1000000000000000000; // 1e18
 const double EPS = 1e-10;
 
 int main(void)
@@ -24,10 +12,9 @@ int main(void)
     int right = 99;
     int counter = 0;
 
-    /* A さんの年齢を 1 つに絞れないうちは繰り返す */
     while (right - left > 1)
     {
-        int mid = left + (right - left) / 2; // 区間の真ん中
+        int mid = left + (right - left) / 2;
 
         cout << "あなたの年齢は " << mid << " 歳ですか？それともそれ以上ですか？ (yes / no)" << endl;
         string ans;
